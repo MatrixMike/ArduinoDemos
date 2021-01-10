e5ace4b5f2918a66c9577252c558ff294fcd3839
Mike on Tecra <matroxmike@icloud.com>
2021-01-10 14:13:07 +1100

    date and board type added for Lilypad

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -2,32 +2,36 @@
   mod 29 April 2015 - demo for RGB LED
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
- 
+
   This example code is in the public domain.
- */
- 
+  for the press-stud Lilypad the green LED is on p13
+  Sun 10 Jan 10:18:01 AEDT 2021
+*/
+
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 9;  // red
-
+int led2 = 13;  // red
+int v1 = 50;
+int v2 = 500;
 // the setup routine runs once when you press reset:
-void setup() {                
+void setup() {
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+  digitalWrite(led2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(v1);               // wait for a second
+  digitalWrite(led2, LOW);    // turn the LED off by making the voltage LOW
+  delay(v2);               // wait for a second
+  digitalWrite(led2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(v1);               // wait for a second
+  digitalWrite(led2, LOW);    // turn the LED off by making the voltage LOW
+  delay(v1);               // wait for a second
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
+  delay(v1);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);               // wait for a second
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);               // wait for a second
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);               // wait for a second
+  delay(v1);               // wait for a second
 }
--- arrayLEDS/arrayLEDS.ino
+++ arrayLEDS/arrayLEDS.ino
@@ -2,16 +2,16 @@ int ledCount = 14;
 int ledPins[] = { 6, 7, 8, 9, 10, 11, 12, 13, 12, 11, 10, 9, 8, 7 };
 int ledDelay = 100;
 
-// 29 April 2015 
+// 29 April 2015
 
 void setup() {
   for (int thisLed = 0; thisLed < ledCount; thisLed++) {
-    pinMode(ledPins[thisLed], OUTPUT); 
+    pinMode(ledPins[thisLed], OUTPUT);
   }
 }
 
 void loop() {
-  for (int thisLed = 0; thisLed < ledCount-1; thisLed++) {
+  for (int thisLed = 0; thisLed < ledCount - 1; thisLed++) {
     digitalWrite(ledPins[thisLed], HIGH);
     delay(ledDelay);
     digitalWrite(ledPins[thisLed], LOW);
